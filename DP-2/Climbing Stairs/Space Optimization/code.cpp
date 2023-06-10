8e9a1f3c94f37cafba58db0d7a5ce7c852492a29
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-06-10 10:00:30 +0530

    Create code.cpp

---
+++ DP-2/Climbing Stairs/Space Optimization/code.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev1=1,prev2=1,curr=0;
+        
+        for(int i=2;i<=n;i++)
+        {
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
